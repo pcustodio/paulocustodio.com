dd0c8fa08f03961c3733a5ec616f7eaae40fd698
Paulo Custódio <paulo.custodio@gmail.com>
2020-06-21 16:49:08 +0100

    new content

--- en/index.php
+++ en/index.php
@@ -32,7 +32,8 @@
                                 <!-- Card -->
                                 <div class="card">
                                     <div class="card-image">
-                                        <a class="lightup" href="../img/folio/nlumi_app.png" title="">
+                                        <a class="lightup" href="../img/folio/nlumi_app.png" title=""
+                                            data-gallery="../img/folio/nlumi_app_02.png">
                                             <img src="../img/folio/thumbs/nlumi_app.png" alt="" title="">
                                         </a>
                                     </div>
